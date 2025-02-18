ceef0d2459996f26f72c130cdd2c5b4de9305225
CopperyTexas <dengleboff@yandex.ru>
2025-02-18 20:30:20 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'Arial', 'sans-serif'],
+        text: ['Raleway', 'Arial', 'sans-serif'],
+      },
+      keyframes: {
+        slideIn: {
+          '0%': { transform: 'translateX(100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        slideIn: 'slideIn 1s ease-out',
+      },
+    },
+  },
+  plugins: [],
+};
