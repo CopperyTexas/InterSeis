3652991129813991008332b4494a9d9703656c3d
Denis Glebov <dengleboff@yandex.ru>
2025-02-19 13:59:33 +0500

    прод сборка работает

--- main.js
+++ main.js
@@ -13,25 +13,34 @@ function createWindow() {
     },
   });
 
-  mainWindow.loadURL('http://localhost:4200');
-  // Открываем DevTools для отладки
-  mainWindow.webContents.openDevTools();
-  mainWindow.on('closed', function () {
+  if (process.env.NODE_ENV === 'development') {
+    // В режиме разработки загружаем Angular Dev Server
+    mainWindow.loadURL('http://localhost:4200');
+    // Опционально открываем DevTools
+    mainWindow.webContents.openDevTools();
+  } else {
+    // В production режиме загружаем статичные файлы из dist
+    const indexPath = path.join(
+      __dirname,
+      'dist',
+      'InterSeis',
+      'browser',
+      'index.html',
+    );
+    mainWindow.loadFile(indexPath);
+  }
+
+  mainWindow.on('closed', () => {
     mainWindow = null;
   });
 }
 
 app.whenReady().then(createWindow);
 
-app.on('window-all-closed', function () {
-  // На macOS приложения обычно не закрывают всё, пока не выйдем явно
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
 });
 
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow();
-  }
+app.on('activate', () => {
+  if (mainWindow === null) createWindow();
 });
