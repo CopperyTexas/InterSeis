5cc61fb579b42e51d99c9fc94c092a0f452f9b74
Denis Glebov <dengleboff@yandex.ru>
2025-03-07 15:50:38 +0500

    header

--- src/app/components/project-window/project-window.component.ts
+++ src/app/components/project-window/project-window.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ProjectWindow } from '../../interfaces/project-window.model';
-import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { MatTab, MatTabGroup, MatTabLabel } from '@angular/material/tabs';
 import { NgForOf } from '@angular/common';
 import { MainPageComponent } from '../main-page/main-page.component';
 import { Subscription } from 'rxjs';
 import { ProjectService } from '../../services/project.service';
-import { MatDialog } from '@angular/material/dialog';
 import { ProjectInfo } from '../../interfaces/project-info.model';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-project-window',
-  imports: [MatTabGroup, MatTab, NgForOf, MainPageComponent],
+  imports: [
+    MatTabGroup,
+    MatTab,
+    NgForOf,
+    MainPageComponent,
+    MatIcon,
+    MatIconButton,
+    MatTooltip,
+    MatTabLabel,
+  ],
   templateUrl: './project-window.component.html',
   styleUrl: './project-window.component.scss',
 })
@@ -19,8 +31,8 @@ export class ProjectWindowComponent implements OnInit, OnDestroy {
   projectWindows: ProjectWindow[] = [
     { id: 1, projectInfo: null },
     { id: 2, projectInfo: null },
-    { id: 3, projectInfo: null },
   ];
+  private nextWindowId = 3;
   private subscription!: Subscription;
 
   constructor(
@@ -29,16 +41,12 @@ export class ProjectWindowComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    // Если вы хотите, чтобы глобальное состояние обновлялось, можно подписаться,
-    // но в нашем случае лучше обновлять только выбранное окно.
+    // Подписываемся на изменения в ProjectService и обновляем только активное окно
     this.subscription = this.projectService.projectInfo$.subscribe(
       (projectInfo) => {
         if (projectInfo) {
-          // Обновляем только активное окно
-          this.updateProjectInfo(
-            this.projectWindows[this.selectedTabIndex].id,
-            projectInfo,
-          );
+          const activeWindow = this.projectWindows[this.selectedTabIndex];
+          this.updateProjectInfo(activeWindow.id, projectInfo);
         }
       },
     );
@@ -48,10 +56,31 @@ export class ProjectWindowComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
+  addTab(): void {
+    // Создаем новое окно
+    const newWindow: ProjectWindow = {
+      id: this.nextWindowId++,
+      projectInfo: null,
+    };
+    this.projectWindows.push(newWindow);
+    // Переключаем фокус на новое окно
+    this.selectedTabIndex = this.projectWindows.length - 1;
+  }
+
+  removeTab(index: number, event: MouseEvent): void {
+    event.stopPropagation(); // отменяем переключение вкладки
+    if (this.projectWindows.length > 1) {
+      this.projectWindows.splice(index, 1);
+      if (this.selectedTabIndex >= this.projectWindows.length) {
+        this.selectedTabIndex = this.projectWindows.length - 1;
+      }
+    }
+  }
+
   updateProjectInfo(windowId: number, projectInfo: ProjectInfo): void {
     const index = this.projectWindows.findIndex((win) => win.id === windowId);
     if (index !== -1) {
-      // Обновляем объект окна с новой информацией
+      // Создаем новый объект, чтобы Angular обнаружил изменение
       this.projectWindows[index] = {
         ...this.projectWindows[index],
         projectInfo,
