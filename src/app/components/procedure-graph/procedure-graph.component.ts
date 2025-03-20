5340d5593b440a514d6af5049c21258e8dea3193
Denis Glebov <dengleboff@yandex.ru>
2025-03-20 18:08:24 +0500

    добавил процедуры

--- src/app/components/procedure-graph/procedure-graph.component.ts
+++ src/app/components/procedure-graph/procedure-graph.component.ts
@@ -8,8 +8,6 @@ import {
 import { JsonPipe, NgForOf } from '@angular/common';
 import { Procedure } from '../../interfaces/procedures/procedure.model';
 import { ProjectInfo } from '../../interfaces/project-info.model';
-import { FftAnalysisParams } from '../../interfaces/procedures/FftAnalysisParams.model';
-import { FilteringParams } from '../../interfaces/procedures/FilteringParams.model';
 import { v4 as uuidv4 } from 'uuid';
 
 @Component({
@@ -51,47 +49,6 @@ export class ProcedureGraphComponent implements OnChanges {
       moveItemInArray(this.procedures, event.previousIndex, event.currentIndex);
     }
 
-    this.syncGraph();
-  }
-
-  addRandomProcedure(): void {
-    const procedureTypes = ['fft-analysis', 'filtering'];
-    const randomType =
-      procedureTypes[Math.floor(Math.random() * procedureTypes.length)];
-
-    let newProcedure: Procedure<any>;
-
-    switch (randomType) {
-      case 'fft-analysis':
-        newProcedure = {
-          id: uuidv4(),
-          type: 'fft-analysis',
-          name: 'Спектральный анализ',
-          parameters: {
-            method: Math.random() > 0.5 ? 'FFT' : 'Wavelet',
-            frequencyLimit: Math.floor(Math.random() * 200) + 50,
-          } as FftAnalysisParams,
-        };
-        break;
-
-      case 'filtering':
-        newProcedure = {
-          id: uuidv4(),
-          type: 'filtering',
-          name: 'Фильтрация сигнала',
-          parameters: {
-            filterType: Math.random() > 0.5 ? 'lowpass' : 'highpass',
-            cutoffFrequency: Math.floor(Math.random() * 100) + 10,
-            order: Math.floor(Math.random() * 4) + 1,
-          } as FilteringParams,
-        };
-        break;
-
-      default:
-        throw new Error('Неизвестный тип процедуры!');
-    }
-
-    this.procedures.push(newProcedure);
     this.syncGraph();
   }
 
